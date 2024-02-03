e6d4e23d3f98dfb0178d8737746bae84b0c06d85
Jakub Czyz <jakub12134@gmail.com>
2024-02-03 14:15:34 +0100

    add number formatting

--- client/src/components/status-bar.vue
+++ client/src/components/status-bar.vue
@@ -4,7 +4,7 @@ import { PropType, computed } from 'vue';
 import { Team, fillColors } from '../domain';
 import Helmet from './helmet.vue';
 
-const props =defineProps({
+const props = defineProps({
     value: {type: Number, required: true},
     height: {type: Number, required: true},
     team: {type: String as PropType<Team>, required: true}
@@ -25,28 +25,8 @@ const maxHeight = 50
 <template>
     <div class="flex flex-col items-center justify-end">
         <div class="w-1 transition-[height] duration-500" :style="`height: ${maxHeight - heightInPx}px;`" ></div>
-        <p class="value pb-1" :style="`--num: ${valueInt}`"></p>
-        <div class="w-14 mb-3 transition-[height] duration-500" :style="`background-color: ${fillColors[team]}; height: ${heightInPx}px;`" ></div>
+        <p class="value pb-1"></p>{{ valueInt.toLocaleString("pl-PL") }}
+        <div class="w-[72px] mb-3 transition-[height] duration-500" :style="`background-color: ${fillColors[team]}; height: ${heightInPx}px;`" ></div>
         <Helmet class="w-12" :team="team" />
     </div>
 </template>
-
-<style scoped>
-
-@property --num {
-  syntax: '<integer>';
-  initial-value: 0;
-  inherits: false;
-}
-
-.value {
-  transition: --num 500ms;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  counter-reset: num var(--num);
-}
-
-.value::after {
-  content: counter(num);
-}
-
-</style>
